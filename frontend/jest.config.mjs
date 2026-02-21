d22015cb77c497c34891dd7980400a2e587fe75d
Rafael Hocevar <hocevar.rafael@gmail.com>
2026-02-21 14:13:12 -0800

    Updated jest config file

--- frontend/jest.config.mjs
+++ frontend/jest.config.mjs
@@ -1,9 +1,8 @@
-import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({ dir: "./" });
 
-const config: Config = {
+const config = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
